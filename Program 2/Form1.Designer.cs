ccfb0308a1eb03d9a47449815018ec465926c866
James Von Vreckin <jcvonv01@louisville.edu>
2017-11-26 14:50:53 -0500

    uploaded Program 2 to github

---
+++ Program 2/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Program_2
+{
+    partial class program2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hoursLabel = new System.Windows.Forms.Label();
+            this.lastNameLabel = new System.Windows.Forms.Label();
+            this.lastNameTxtBox = new System.Windows.Forms.TextBox();
+            this.hoursTxtBox = new System.Windows.Forms.TextBox();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dayLabel = new System.Windows.Forms.Label();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // hoursLabel
+            // 
+            this.hoursLabel.AutoSize = true;
+            this.hoursLabel.Location = new System.Drawing.Point(12, 45);
+            this.hoursLabel.Name = "hoursLabel";
+            this.hoursLabel.Size = new System.Drawing.Size(187, 13);
+            this.hoursLabel.TabIndex = 0;
+            this.hoursLabel.Text = "Hours Completed Prior to Spring 2016:";
+            // 
+            // lastNameLabel
+            // 
+            this.lastNameLabel.AutoSize = true;
+            this.lastNameLabel.Location = new System.Drawing.Point(36, 92);
+            this.lastNameLabel.Name = "lastNameLabel";
+            this.lastNameLabel.Size = new System.Drawing.Size(163, 13);
+            this.lastNameLabel.TabIndex = 1;
+            this.lastNameLabel.Text = "Last Name ( Capitalize first letter):";
+            // 
+            // lastNameTxtBox
+            // 
+            this.lastNameTxtBox.Location = new System.Drawing.Point(205, 89);
+            this.lastNameTxtBox.Name = "lastNameTxtBox";
+            this.lastNameTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.lastNameTxtBox.TabIndex = 2;
+            // 
+            // hoursTxtBox
+            // 
+            this.hoursTxtBox.Location = new System.Drawing.Point(205, 42);
+            this.hoursTxtBox.Name = "hoursTxtBox";
+            this.hoursTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.hoursTxtBox.TabIndex = 3;
+            // 
+            // enterButton
+            // 
+            this.enterButton.Location = new System.Drawing.Point(124, 188);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(75, 23);
+            this.enterButton.TabIndex = 4;
+            this.enterButton.Text = "Enter";
+            this.enterButton.UseVisualStyleBackColor = true;
+            this.enterButton.Click += new System.EventHandler(this.enterButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Registration Availablity";
+            // 
+            // dayLabel
+            // 
+            this.dayLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.dayLabel.Location = new System.Drawing.Point(31, 147);
+            this.dayLabel.Name = "dayLabel";
+            this.dayLabel.Size = new System.Drawing.Size(153, 23);
+            this.dayLabel.TabIndex = 6;
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.timeLabel.Location = new System.Drawing.Point(190, 147);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(100, 23);
+            this.timeLabel.TabIndex = 7;
+            // 
+            // program2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 223);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.dayLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.hoursTxtBox);
+            this.Controls.Add(this.lastNameTxtBox);
+            this.Controls.Add(this.lastNameLabel);
+            this.Controls.Add(this.hoursLabel);
+            this.Name = "program2";
+            this.Text = "Program 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label hoursLabel;
+        private System.Windows.Forms.Label lastNameLabel;
+        private System.Windows.Forms.TextBox lastNameTxtBox;
+        private System.Windows.Forms.TextBox hoursTxtBox;
+        private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label dayLabel;
+        private System.Windows.Forms.Label timeLabel;
+    }
+}
+
---
+++ Program 2/Form1.cs
@@ -0,0 +1,164 @@
+﻿//Codey Von Vreckin
+// CIS 199-01
+// Program 2
+// March 6th, 2016
+// This program asks the user to input their current number of completed credit hours as well as their last name. 
+// Once entered the program displays the day and time the user may register their classes for the upcoming  Fall 2016 Semester.
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_2
+{
+    public partial class program2 : Form
+    {
+        public program2()
+        {
+            InitializeComponent();
+        }
+
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            double hours; //declares the double variable hours to hold the number of completed credit hours
+            char lastName;// declares the char variable to hold the last name of the student
+            const double SENIOR = 90;//declares a constant double variable to hold the # of hours required to be a Senior
+            const double JUNIOR = 60;//declares a constant double variable to hold the # of hours required to be a Junior
+            const double SOPHOMORE = 30;//declares a constant double variable to hold the # of hours required to be a Sophomore
+            const string SENIOR_DAY = "Wednesday, March 30th";// declares the constant string variable to hold the day seniors are able to register for classes
+            const string JUNIOR_DAY = "Thursday, March 31st";// declares the constant string variable to hold the day juniors are able to register for classes
+            const string SOPHOMORE_DAY_1 = "Friday, April 1st";// declares the constant string variable to hold the first day sophomores are able to register for classes
+            const string SOPHOMORE_DAY_2 = "Monday, April 4th";// declares the constant string variable to hold the second day sophomores are able to register for classes
+            const string FRESHMAN_DAY_1 = "Tuesday, April 5th";// declares the constant string variable to hold the first day freshman are able to register for classes
+            const string FRESHMAN_DAY_2 = "Wednesday, April 6th";// declares the constant string variable to hold the second day freshman are able to register for classes
+            const string TIME_1 = "8:30 A.M"; //declares the constant string variable to hold the 1st time available to register for classes
+            const string TIME_2 = "10:00 A.M";//declares the constant string variable to hold the 2nd time available to register for classes
+            const string TIME_3 = "11:30 A.M";//declares the constant string variable to hold the 3rd time available to register for classes
+            const string TIME_4 = "2:00 P.M";//declares the constant string variable to hold the 4th time available to register for classes
+            const string TIME_5 = "4:00 P.M";//declares the constant string variable to hold the 5th time available to register for classes
+            lastName = lastNameTxtBox.Text[0]; //pulls out the first letter of the students last name
+           
+            if (double.TryParse(hoursTxtBox.Text, out hours) && hours > 0) // if statment that attempts to parse the content inputed into the hours text box
+
+                if (hours >= SENIOR) // if statment that asks if the number of hours are greater than or equal to 90
+                {
+                    dayLabel.Text = SENIOR_DAY;// displays the day seniors can reigster for classes in the dayLabel
+                    if (lastName >= 'E' && lastName <= 'I') //asks if the first letter of the last name is between E and I                   
+                        timeLabel.Text = TIME_1;// displays the 1st time available to register in the timeLabel
+
+                    else if (lastName >= 'J' && lastName <= 'O')//asks if the first letter of the last name is between J and O  
+                        timeLabel.Text = TIME_2;// displays the 2nd time available to register in the timeLabel
+
+                    else if (lastName >= 'P' && lastName <= 'S')//asks if the first letter of the last name is between P and S  
+                        timeLabel.Text = TIME_3;// displays the 3rd time available to register in the timeLabel
+
+                    else if (lastName >= 'T' && lastName <= 'Z')//asks if the first letter of the last name is between T and Z  
+                        timeLabel.Text = TIME_4;// displays the 4th time available to register in the timeLabel
+                    
+                    else
+                        timeLabel.Text = TIME_5; // displays the 5th time available to register in the timeLabel               
+                }
+                else if (hours >= JUNIOR && hours < SENIOR) // if statment that asks if the number of hours are between 60 and 90
+                {
+                    dayLabel.Text = JUNIOR_DAY;// displays the day juniors can reigster for classes in the dayLabel
+                    if (lastName >= 'E' && lastName <= 'I')//asks if the first letter of the last name is between E and I  
+                        timeLabel.Text = TIME_1;// displays the 1st time available to register in the timeLabel
+
+                    else if (lastName >= 'J' && lastName <= 'O')//asks if the first letter of the last name is between J and O  
+                        timeLabel.Text = TIME_2;// displays the 2nd time available to register in the timeLabel
+
+                    else if (lastName >= 'P' && lastName <= 'S')//asks if the first letter of the last name is between P and S  
+                        timeLabel.Text = TIME_3;// displays the 3rd time available to register in the timeLabel
+
+                    else if (lastName >= 'T' && lastName <= 'Z')//asks if the first letter of the last name is between T and Z  
+                        timeLabel.Text = TIME_4;// displays the 4th time available to register in the timeLabel
+                    
+                    else
+                        timeLabel.Text = TIME_5;// displays the 5th time available to register in the timeLabel               
+                }
+                else if (hours >= SOPHOMORE && hours < JUNIOR)// if statment that asks if the number of hours are between 30 and 60
+                {
+                    if (lastName >= 'E' && lastName <= 'Q')//asks if the first letter of the last name is between E and Q  
+                    {
+                        dayLabel.Text = SOPHOMORE_DAY_1;// displays the 1st day sophomores can reigster for classes in the dayLabel
+                        if (lastName >= 'E' && lastName <= 'F')//asks if the first letter of the last name is between E and F  
+                            timeLabel.Text = TIME_1;// displays the 1st time available to register in the timeLabel
+
+                        else if (lastName >= 'G' && lastName <= 'I')//asks if the first letter of the last name is between G and I  
+                            timeLabel.Text = TIME_2;// displays the 2nd time available to register in the timeLabel
+
+                        else if (lastName >= 'J' && lastName <= 'L')//asks if the first letter of the last name is between J and L  
+                            timeLabel.Text = TIME_3;// displays the 3rd time available to register in the timeLabel
+
+                        else if (lastName >= 'M' && lastName <= 'O')//asks if the first letter of the last name is between M and O  
+                            timeLabel.Text = TIME_4;// displays the 4th time available to register in the timeLabel
+                        
+                        else
+                            timeLabel.Text = TIME_5;// displays the 5th time available to register in the timeLabel
+                    }
+                    else// performs if the first letter of the name was not between E and Q
+                    {
+                        dayLabel.Text = SOPHOMORE_DAY_2;// displays the 2nd day sophomores can reigster for classes in the dayLabel
+                        if (lastName >= 'R' && lastName <= 'S')//asks if the first letter of the last name is between R and S  
+                            timeLabel.Text = TIME_1;// displays the 1st time available to register in the timeLabel
+                        
+                        else if (lastName >= 'T' && lastName <= 'V')//asks if the first letter of the last name is between T and V  
+                            timeLabel.Text = TIME_2;// displays the 2nd time available to register in the timeLabel
+                        
+                        else if (lastName >= 'W' && lastName <= 'Z')//asks if the first letter of the last name is between W and Z  
+                            timeLabel.Text = TIME_3;// displays the 3rd time available to register in the timeLabel
+                        
+                        else if (lastName >= 'A' && lastName <= 'B')//asks if the first letter of the last name is between A and B  
+                            timeLabel.Text = TIME_4;// displays the 4th time available to register in the timeLabel
+                        else
+                            timeLabel.Text = TIME_5;// displays the 5th time available to register in the timeLabel
+                    }
+                }
+                else// performs if the number of hours are less than 30
+                {
+                    if (lastName >= 'E' && lastName <= 'Q')//asks if the first letter of the last name is between E and Q  
+                    {
+                        dayLabel.Text = FRESHMAN_DAY_1;// displays the 1st day freshman can reigster for classes in the dayLabel
+                        if (lastName >= 'E' && lastName <= 'F')//asks if the first letter of the last name is between E and F  
+                            timeLabel.Text = TIME_1;// displays the 1st time available to register in the timeLabel
+
+                        else if (lastName >= 'G' && lastName <= 'I')//asks if the first letter of the last name is between G and I  
+                            timeLabel.Text = TIME_2;// displays the 2nd time available to register in the timeLabel
+
+                        else if (lastName >= 'J' && lastName <= 'L')//asks if the first letter of the last name is between J and L  
+                            timeLabel.Text = TIME_3;// displays the 3rd time available to register in the timeLabel
+
+                        else if (lastName >= 'M' && lastName <= 'O')//asks if the first letter of the last name is between M and O  
+                            timeLabel.Text = TIME_4;// displays the 4th time available to register in the timeLabel
+
+                        else
+                            timeLabel.Text = TIME_5;// displays the 5th time available to register in the timeLabel
+                    }
+                    else// performs if the first letter of the name was not between E and Q
+                    {
+                        dayLabel.Text = FRESHMAN_DAY_2;// displays the 2nd day freshman can reigster for classes in the dayLabel
+                        if (lastName >= 'R' && lastName <= 'S')//asks if the first letter of the last name is between R and S  
+                            timeLabel.Text = TIME_1;// displays the 1st time available to register in the timeLabel
+
+                        else if (lastName >= 'T' && lastName <= 'V')//asks if the first letter of the last name is between T and V  
+                            timeLabel.Text = TIME_2;// displays the 2nd time available to register in the timeLabel
+
+                        else if (lastName >= 'W' && lastName <= 'Z')//asks if the first letter of the last name is between W and Z  
+                            timeLabel.Text = TIME_3;// displays the 3rd time available to register in the timeLabel
+
+                        else if (lastName >= 'A' && lastName <= 'B')//asks if the first letter of the last name is between A and B  
+                            timeLabel.Text = TIME_4;// displays the 4th time available to register in the timeLabel
+                        else
+                            timeLabel.Text = TIME_5;// displays the 5th time available to register in the timeLabel
+                    }
+                }
+            else// performs if there is nothing entered in the hours text box or if the number entered is a negative
+                MessageBox.Show("Invaild data was entered");// displays an error message informing the user that there was invaild data entered
+        }
+    }
+}
